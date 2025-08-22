8e41a328eaef5c774434cfc0c71f15a70470dbb9
LUIZ GUSTAVO DE JESUS ARAUJO <25162429@edu119.local>
2025-08-22 16:25:41 -0300

    Adicionando o post

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const ALUNOS = [
         id: 1, nome: "Vitor Lima", cor: "Azul", idade: 26
     },
     {
-        id: 2, nome: "João Gamer", cor: "Preto",  idade: 17
+        id: 2, nome: "João Gamer", cor: "Preto", idade: 17
     },
     {
-        id: 3, nome: "Henry Sem ducha",cor: "Verde",idade: 19
+        id: 3, nome: "Henry Sem ducha", cor: "Verde", idade: 19
     },
 ]
 
@@ -23,25 +23,25 @@ app.get("/", (req, res) => {
     })
 })
 
-app.get("/alunos",(req, res)=>{
+app.get("/alunos", (req, res) => {
     res.json(ALUNOS);
 })
 
-app.get("/alunos/:id", (req , res)=>{
+app.get("/alunos/:id", (req, res) => {
     const id = Number(req.params.id)
     console.log(`Valor recebido ${id}`);
-    
-    const aluno = ALUNOS.filter( (aluno) => aluno.id === id )
-    if(aluno.length > 0){
+
+    const aluno = ALUNOS.filter((aluno) => aluno.id === id)
+    if (aluno.length > 0) {
         res.status(200).json(aluno)
-    }else{
-        res.status(404).json({ msg: "Aluno não encontrado"})
-    }     
+    } else {
+        res.status(404).json({ msg: "Aluno não encontrado" })
+    }
 })
 
 app.get("/alunos/cor/:cor", (req, res) => {
     const cor = req.params.cor;
-    console.log(`Cor recebida: ${cor}`);    
+    console.log(`Cor recebida: ${cor}`);
     const alunosFiltrados = ALUNOS.filter(
         (aluno) => aluno.cor.toLowerCase() === cor.toLowerCase()
     );
@@ -51,7 +51,25 @@ app.get("/alunos/cor/:cor", (req, res) => {
         res.status(404).json({ msg: "Nenhum aluno encontrado com essa cor" });
     }
 });
+app.post("/alunos", (req, res) => {
+    const { nome, cor, idade } = req.body;
+    if (!nome || !cor || !idade) {
+        return res.status(400).json({ msg: "Nome cor e idade são obrigatórios" })
+    }
+    // ALUNOS.length = 3
+    // ALUNOS[2]
+    // ALUNOS.length - 1
+    // ALUNOS[2].id = 3
+    // ALUNOS[2].id + 1
+    // id = 4
+    const id = ALUNOS.length > 0 ? ALUNOS[ALUNOS.length - 1].id + 1 : 1;
 
+    const novoAluno = {
+        id, nome, cor, idade
+    }
+    ALUNOS.push(novoAluno);
+    res.status(201).json({ msg: "Aluno criado com sucesso" })
+})
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 })
