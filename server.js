2f111d1cfc8ef8c4f90edc6cc8c13e4f690aa1f9
LUIZ GUSTAVO DE JESUS ARAUJO <25162429@edu119.local>
2025-08-22 16:50:28 -0300

    ADICIONANDO O POST

--- server.js
+++ server.js
@@ -70,6 +70,17 @@ app.post("/alunos", (req, res) => {
     ALUNOS.push(novoAluno);
     res.status(201).json({ msg: "Aluno criado com sucesso" })
 })
+app.delete("/alunos/:id", (req, res) => {
+     const id = Number(req.params.id); 
+     const indice = ALUNOS.findIndex((aluno) => aluno.id === id)
+
+     if (indice === -1) 
+        return res.status(404).json({msg: "Aluno não encontrado"});
+     
+     console.log(indice)
+     ALUNOS.splice(indice, 1);
+     res.status(204).json({msg: "Aluno deletado com sucesso!"});
+})
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 })
